158a844207b420415e8be25e0b4c70c1bf1daf56
bilorukavsky <90616723+bilorukavsky@users.noreply.github.com>
2022-11-15 11:37:25 +0300

    Update main

--- repeating_bot/main.py
+++ repeating_bot/main.py
@@ -1,8 +1,17 @@
 import logging
 import os
 
-from telegram import Update, ForceReply
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram import Update, ForceReply, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    CallbackContext,
+    CallbackQueryHandler,
+    ContextTypes
+)
+
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
@@ -12,7 +21,14 @@ logger = logging.getLogger(__name__)
 
 
 def start(update: Update, context: CallbackContext) -> None:
+    keyboard = [
+        [InlineKeyboardButton(text = "Add a photo", callback_data='1')],
+        [InlineKeyboardButton(text = "GitHub", url = 'https://github.com/bilorukavsky/python_task')],
+        [InlineKeyboardButton(text = "Quote", callback_data='2')]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
     update.message.reply_text('Hi!')
+    update.message.reply_text('Please choose:', reply_markup=reply_markup)
 
 
 def help_command(update: Update, context: CallbackContext) -> None:
@@ -22,6 +38,22 @@ def help_command(update: Update, context: CallbackContext) -> None:
 def echo(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(update.message.text)
 
+    
+def photo(update: Update, context: CallbackContext) -> None:
+    photo_file = update.message.photo.get_file()
+    photo_file.download('user_photo.jpg')
+    logger.info('user_photo.jpg')
+    update.message.reply_text('Add to photo')
+
+
+def button(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
+    if (query.data== '1'):
+        photo(query,context)
+    elif(query.data == '2'):
+       query.edit_message_text("Quote")
+    query.answer()
+
 
 def main() -> None:
     updater = Updater(os.environ["TOKEN"])
@@ -30,6 +62,7 @@ def main() -> None:
 
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(CallbackQueryHandler(button))
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
     updater.start_polling()
